787a8245582fa456845a4c351e526d8c6377307a
Jibaru <ignacioruedaboada@hotmail.com>
2022-12-05 11:22:16 -0500

    feat: add template.py

---
+++ template.py
@@ -0,0 +1,18 @@
+'''
+Description 01
+'''
+def solve01(data):
+    pass
+
+'''
+Description 02
+'''
+def solve02(data):
+    pass
+    
+if __name__ == "__main__":
+    data = open('day-n-input.test.txt', 'r').read()
+    # data = open('day-n-input.txt', 'r').read()
+
+    print(solve01(data))
+    print(solve02(data))
